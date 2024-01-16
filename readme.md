879cb2fd8d7fdd2ce8630d48db37dab9477f2ef1
zhaozhentao <qq344696734@gmail.com>
2024-01-16 17:04:07 +0800

    route

---
+++ readme.md
@@ -0,0 +1,5 @@
+触发更新 route 。
+
+```shell
+curl -X POST http://localhost:8081/actuator/refresh
+```
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,27 +10,36 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class DemoApplication {
 
+    private Integer time = 1;
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
     @Bean
+    @RefreshScope
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-            .route(
-                r -> r.predicate(serverWebExchange -> {
-                    var queryParams = serverWebExchange.getRequest().getQueryParams();
-                    var color = queryParams.get("color");
-                    return color.contains("red");
-                }).uri("ws://127.0.0.1:3000")
-            )
-            .route(
-                r -> r.predicate(serverWebExchange -> {
-                    var queryParams = serverWebExchange.getRequest().getQueryParams();
-                    var color = queryParams.get("color");
-                    return color.contains("green");
-                }).uri("ws://127.0.0.1:3001")
-            )
-            .build();
+        if (time == 1) {
+            time++;
+            return builder.routes()
+                .route(
+                    r -> r.predicate(serverWebExchange -> {
+                        var queryParams = serverWebExchange.getRequest().getQueryParams();
+                        var color = queryParams.get("color");
+                        return color.contains("red");
+                    }).uri("ws://127.0.0.1:3000")
+                )
+                .build();
+        } else {
+            return builder.routes()
+                .route(
+                    r -> r.predicate(serverWebExchange -> {
+                        var queryParams = serverWebExchange.getRequest().getQueryParams();
+                        var color = queryParams.get("color");
+                        return color.contains("yellow");
+                    }).uri("ws://127.0.0.1:3001")
+                )
+                .build();
+        }
     }
 }
