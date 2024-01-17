11155857c945b152f1c305f1d2cb84552b4db5a6
zhaozhentao <qq344696734@gmail.com>
2024-01-17 10:42:23 +0800

    ooo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,8 +11,6 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class DemoApplication {
 
-    private Integer time = 1;
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
@@ -20,29 +18,21 @@ public class DemoApplication {
     @Bean
     @RefreshScope
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        if (time == 1) {
-            time++;
-            return builder.routes()
-                .route(
-                    r -> r.predicate(serverWebExchange -> {
-                            var queryParams = serverWebExchange.getRequest().getQueryParams();
+        return builder.routes()
+            .route(
+                r -> r.predicate(serverWebExchange -> {
+                        var queryParams = serverWebExchange.getRequest().getQueryParams();
 
-                            return CollUtil.contains(queryParams.get("color"), "red");
-                        })
-                        .uri("ws://127.0.0.1:3000")
-                )
-                .build();
-        } else {
-            return builder.routes()
-                .route(
-                    r -> r.predicate(serverWebExchange -> {
-                            var queryParams = serverWebExchange.getRequest().getQueryParams();
+                        var devIds = queryParams.get("devId");
 
-                            return CollUtil.contains(queryParams.get("color"), "yellow");
-                        })
-                        .uri("ws://127.0.0.1:3001")
-                )
-                .build();
-        }
+                        if (CollUtil.isEmpty(devIds)) return false;
+
+                        var devId = devIds.get(0);
+
+                        return devId.hashCode() % 2 == 0;
+                    })
+                    .uri("ws://127.0.0.1:8012")
+            )
+            .build();
     }
 }
